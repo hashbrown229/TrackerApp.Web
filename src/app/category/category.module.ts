93e50a09670b015f516403af3ef111f1fe59213e
Harsh Gandhi <harshgandhi229@gmail.com>
2024-02-19 19:01:53 -0500

    Blank category module added. Future Feature

---
+++ src/app/category/category.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoryListComponent } from './components';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CategoryListComponent]
+})
+export class CategoryModule { }
