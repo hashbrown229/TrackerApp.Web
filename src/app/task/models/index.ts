147d635ed91e1e0c6627f5b106eaa87a73284d68
Harsh Gandhi <harshgandhi229@gmail.com>
2024-02-19 19:00:59 -0500

    Indexes updated

---
+++ src/app/task/models/index.ts
@@ -0,0 +1,5 @@
+export { TaskDTO } from './TaskDTO';
+export { CreateTaskDTO } from './CreateTaskDTO';
+export { UpdateTaskDTO } from './UpdateTaskDTO';
+export { PaginationDTO } from './PaginationDTO';
+export { PRIORITY, STATUS, CATEGORY } from './Utils';
